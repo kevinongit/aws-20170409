247bf6a58f70fb40f68edec0ce18773d1a43c86f
kevin Hwang <kevin@kevinui-MacBook-Pro.local>
2017-04-19 16:50:05 +0900

    header and footer added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
 
 /**
 * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -11,8 +12,8 @@ import { HeaderComponent } from './header/header.component'
 
 @NgModule({
     imports: [CommonModule, RouterModule],
-    declarations: [ HeaderComponent ],
-    exports: [CommonModule, FormsModule, RouterModule, HeaderComponent]
+    declarations: [ HeaderComponent, FooterComponent ],
+    exports: [CommonModule, FormsModule, RouterModule, HeaderComponent, FooterComponent]
 })
 export class SharedModule {
     static forRoot(): ModuleWithProviders {
